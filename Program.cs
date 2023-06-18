2a479098ce72de31f2c1d997a19a8e1fc9ea8ee8
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-18 23:11:26 +0530

    [Divya]:Welcome to Oops Concept

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OopsConcept
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Write a code with OOps concept");
+
+            Constructor Con = new Constructor();
+            Constructor Con2 = new Constructor(52, 8896);
+            Constructor Con3 = new Constructor(55, 996, 75);
+        }
+    }
+}
