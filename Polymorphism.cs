d5e7f2c1d3a15ae7b141970c4ab36099e126c582
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-23 15:27:37 +0530

    [Divya]: Write a code of Polymorphism

---
+++ Polymorphism.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    //Method OverLoading
+    public class Polymorphism
+    {
+        int a, b, c, d;
+       public int Add(int a,int b)
+       {
+            return  d = a + b;
+       }
+
+        public int Add (int a, int b , int c)
+        {
+            return d = a + b + c;
+        }
+    }
+}
