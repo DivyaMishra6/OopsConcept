0a5e50c6a3a752cf771b92dd11108a33de1bb856
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-23 15:15:49 +0530

    [Divya]: run Oops concept

--- Program.cs
+++ Program.cs
@@ -6,9 +6,48 @@
         {
             Console.WriteLine("Write a code with OOps concept");
 
-            Constructor Con = new Constructor();
-            Constructor Con2 = new Constructor(52, 8896);
-            Constructor Con3 = new Constructor(55, 996, 75);
+            //Constructor Con = new Constructor();
+            //Constructor Con2 = new Constructor(52, 8896);
+            //Constructor Con3 = new Constructor(55, 996, 75);
+
+
+
+            //ClassAndObj obj = new ClassAndObj();
+            //obj.Add();
+            //obj.priya();
+
+
+            //Console.WriteLine("!!!!Welcome to Abstraction!!!!");
+
+            //Rectangle R = new Rectangle(12.98, 56.78);
+            //Circle C = new Circle(87.90);
+
+            //Console.WriteLine("Area of Rectangle :" + R.GetArea());
+            //Console.WriteLine("Area of Circle :" + C.GetArea());
+
+
+            //for interface
+            //IBank obj1 = new PunjabBank();
+            //obj1.Operation();
+            //obj1.NameOfBank();
+
+            //Society obj2 = new PunjabBank();
+            //obj2.Operation();
+
+            //!!!!!Polymorphism
+
+            //Polymorphism pol = new Polymorphism();
+            //pol.Add(20,40);
+            //pol.Add(10, 40, 20);
+            //Console.WriteLine("Result :" );
+
+            //Encapsulation
+
+            Console.WriteLine("Welcomw to Encapsulation program");
+
+            Encapsulation Cap = new Encapsulation();
+            Cap.Name = "Divya";
+            Cap.Age = 25;
         }
     }
 }
