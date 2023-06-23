455e251acf84cda0c345794558be68ca0f92ac45
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-23 15:33:30 +0530

    [Divya]: Write a code for Interface

---
+++ Interface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public interface IBank
+    {
+        public void NameOfBank();
+        public void Operation();
+
+    }
+
+    public interface Society
+    {
+        public void Operation();
+    }
+
+    public class PunjabBank : IBank, Society
+    {
+        void IBank.Operation()
+        {
+            Console.WriteLine("Operation Of Bank");
+
+        }
+        void Society.Operation()
+        {
+            Console.WriteLine("Operation Of Society");
+        }
+
+        public void NameOfBank()
+        {
+            Console.WriteLine("Override the Name of Bank");
+        }
+
+    }
+    
+}
