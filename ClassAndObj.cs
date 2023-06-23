67e7a9b38f855f750cfd28577798d30b5b6a135f
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-23 15:21:32 +0530

    [Divya]: Write a code class and obj

---
+++ ClassAndObj.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class ClassAndObj
+    {
+        int num1 = 40;
+        int num2 = 20;
+
+        //Method or Behaviour
+
+        public void Add()
+        {
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+
+        public void Sub()
+        {
+            int result = num1 - num2;
+            Console.WriteLine(result);
+        }
+
+        public void priya()
+        {
+            ClassAndObj obj = new ClassAndObj();
+            obj.num1 = 20;
+            obj.num2 = 10;
+            obj.Sub();
+        }
+
+    }
+}
